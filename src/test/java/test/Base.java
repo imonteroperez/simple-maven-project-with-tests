100746895bdd4659d7ebb2b1debf8e74d59c1d1f
Ildefonso Montero <imontero@cloudbees.com>
2021-11-26 12:22:28 +0100

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,11 +30,11 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random();
-        if (r < 0.1) {
-            fail("oops");
-        } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
+        int min = 1;
+        int max = 10;
+        int random = new java.util.Random().nextInt((max - min) + 1) + min;
+        if (random % 2 == 0) {
+           fail("oops"); 
         }
     }
 
